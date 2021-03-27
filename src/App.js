92cbfc916e28ba0a076462b8dfa7f10ae91ca374
Mounisa <mounisa.periyasamy@ameexusa.com>
2021-03-27 10:20:20 +0530

    react add-delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    cart: []
+  };
+
+  saveInput = (e) => {
+    this.setState({ input: e.target.value });
+  };
+  removeInput = (sIndex) => {
+    this.setState({ id: sIndex });
+  };
+
+  addNewItem = () => {
+    let { cart, input } = this.state;
+    cart.push(input);
+    this.setState(cart);
+    // this.state.cart.push(this.state.input); // same as above, though bad practice
+  };
+  handleRemove = (sIndex) => {
+    const cart = [...this.state.cart];
+    //this.setState({ cart: this.state.cart.splice(sIndex, 1) });
+    cart.splice(sIndex, 1);
+    this.setState({ cart: cart });
+  };
+
+  render() {
+    return (
+      <div class="react-form">
+        <h1>First Add To Do</h1>
+        <input type="text" onChange={this.saveInput} />
+        <button onClick={this.addNewItem}> Add Item </button>
+        <table>
+          {this.state.cart.map((subItems, sIndex) => {
+            return (
+              <tr key={sIndex} onChange={this.removeInput}>
+                {" "}
+                {subItems}
+                <button onClick={() => this.handleRemove(sIndex,subItems)}>Remove</button>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
